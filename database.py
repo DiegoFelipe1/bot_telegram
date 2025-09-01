0c61e46ceb0e4f896fe2bde4d9493f0af261d5ec
Diego Felipe <fdiego039@gmail.com>
2025-09-01 19:05:47 -0300

    Atualizando o banco de dados

--- database.py
+++ database.py
@@ -1,17 +1,20 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime,BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-engine = create_engine("sqlite:///bot_database.db")
+DB_DSN = "postgresql://botuser:5JaZI1REfMd3y1WUrpDXLEeGfFi1ddNI@dpg-d2r15undiees73dnvnb0-a/botdb_oefx"
+engine = create_engine(DB_DSN)
 SessionLocal = sessionmaker(bind=engine)
 
+
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 class UserAccess(Base):
     __tablename__ = "user_access"
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True, index=True)
+    telegram_id = Column(BigInteger, unique=True, index=True)
     full_name = Column(String)
     is_paid_client = Column(Boolean, default=False)
     subscription_date = Column(DateTime, nullable=True)
